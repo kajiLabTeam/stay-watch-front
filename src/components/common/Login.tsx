5146f77089f84b702cb359f9ad653afe77f37108
happy663 <tatu2425@gmail.com>
2022-07-25 13:42:32 +0900

    login動作設定

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -1,7 +1,12 @@
+import { login } from "@/utils/Auth";
+
 const Login = () => {
   return (
     <div>
-      <button className="p-1 bg-slate-500 rounded-md hover:opacity-90">
+      <button
+        className="p-1 bg-slate-500 rounded-md hover:opacity-90"
+        onClick={login}
+      >
         Log in
       </button>
     </div>
