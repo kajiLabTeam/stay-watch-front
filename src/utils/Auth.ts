3743217db647f49c8a658099b90cad42d85890eb
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-09 02:34:52 +0900

    add:recoilでroleのstateを管理できるように

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosResponse } from "axios";
 import firebase, {
   getAuth,
   GoogleAuthProvider,
@@ -7,6 +8,7 @@ import firebase, {
 } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { atom, useRecoilValue, useSetRecoilState } from "recoil";
+import { baseURL } from "./api";
 import { app } from "@/utils/firebase";
 
 type UserState = firebase.User | null;
@@ -16,8 +18,13 @@ export const userState = atom<UserState>({
   default: null,
   dangerouslyAllowMutability: true,
 });
-
 export default userState;
+
+export const userRole = atom<number | null>({
+  key: "userRole",
+  default: null,
+  dangerouslyAllowMutability: true,
+});
 
 export const login = (): Promise<void> => {
   const provider = new GoogleAuthProvider();
@@ -49,4 +56,40 @@ export const useAuth = (): boolean => {
 
 export const useUser = (): UserState => {
   return useRecoilValue(userState);
+};
+
+export const useUserRole = (): number | null => {
+  return useRecoilValue(userRole);
+};
+
+export const useAuthEmail = (): boolean => {
+  const [isRegisteredEmail, setIsRegisteredEmail] = useState(false);
+  const setUserRole = useSetRecoilState(userRole);
+  const user = useUser();
+
+  useEffect(() => {
+    if (user) {
+      const checkRegisterdEmail = async () => {
+        try {
+          const token = await user.getIdToken();
+          const resRole: AxiosResponse<number> = await axios.get(
+            `${baseURL}/user/v1/check`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setIsRegisteredEmail(true);
+          setUserRole(resRole.data);
+        } catch (error) {
+          setIsRegisteredEmail(false);
+        }
+      };
+
+      checkRegisterdEmail();
+    }
+  }, [user]);
+
+  return isRegisteredEmail;
 };
