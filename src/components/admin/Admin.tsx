fb46c8953b5d80de8d42940436c5a2aa8c59fd70
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-18 01:05:31 +0900

    add: 管理者画面コンポーネントの作成

---
+++ src/components/admin/Admin.tsx
@@ -0,0 +1,33 @@
+import { Tabs } from "@mantine/core";
+import { EmailForm } from "./EmilForm";
+
+export const Admin = () => {
+  return (
+    <div className=" flex h-screen justify-center">
+      <div className="w-1/2 ">
+        <div className="my-2 text-center text-3xl font-bold">
+          メンバーの招待
+        </div>
+        <div className=" border border-black" />
+        <div className="mt-10 rounded-lg bg-slate-200">
+          <Tabs defaultValue="outline">
+            <Tabs.List>
+              <Tabs.Tab value="gallery" className="w-6/12">
+                BLEビーコン登録済み
+              </Tabs.Tab>
+              <Tabs.Tab value="messages" className="w-6/12">
+                BLEビーコン未登録
+              </Tabs.Tab>
+            </Tabs.List>
+            <Tabs.Panel value="gallery" pt="xs">
+              Gallery tab content
+            </Tabs.Panel>
+            <Tabs.Panel value="messages" pt="xs">
+              <EmailForm />
+            </Tabs.Panel>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
+};
