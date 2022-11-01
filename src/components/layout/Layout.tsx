a093a1820f410fd95a87ca72a953fe49dfe29087
togawa427 <ssinya0922@gmail.com>
2022-11-01 15:52:38 +0900

    デスクトップとモバイルの切り替え機能
    デスクトップとモバイルで呼び出すコンポーネントが切り替わるようにした。

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,42 @@
 import { ReactNode } from "react";
-import Header from "@/components/layout/Header";
+import { useWindowSize } from "usehooks-ts";
+import Head from "@/components/layout/Header";
+import Foot from "@/components/layout/Footer";
 
 const Layout = ({ children }: { children?: ReactNode }) => {
-  return (
-    <div>
-      <Header />
-      <main className="mx-auto w-11/12  pb-20 md:w-4/5 md:pb-2">
-        {children}
-      </main>
-    </div>
-  );
+  const { width, height } = useWindowSize();
+
+  // デスクトップの場合
+  if (width > 853){
+    return(
+      <div>
+        <Head />
+        <main className="mx-auto w-11/12  pb-20 md:w-4/5 md:pb-2">
+          {children}
+        </main>
+      </div>
+    );
+  // モバイルの場合
+  } else {
+    return(
+      <div>
+        <Foot />
+        <main className="mx-auto w-11/12  pb-20 md:w-4/5 md:pb-2">
+          {children}
+        </main>
+      </div>
+    );
+  }
+
+  // return (
+  //   <div>
+  //     {/* 条件分岐でFooter.tsxを呼ぶように */}
+  //     <Head />
+  //     <main className="mx-auto w-11/12  pb-20 md:w-4/5 md:pb-2">
+  //       {children}
+  //     </main>
+  //   </div>
+  // );
 };
 
 export default Layout;
