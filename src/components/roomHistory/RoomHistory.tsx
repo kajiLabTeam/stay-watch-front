d8c68b23358a5b19ebeeb2fac5b55454069a3891
happy663 <tatu2425@gmail.com>
2023-03-16 19:06:18 +0900

    fix: fixコマンドによる自動修正

--- src/components/roomHistory/RoomHistory.tsx
+++ src/components/roomHistory/RoomHistory.tsx
@@ -123,8 +123,8 @@ const RoomHistory = () => {
         if (width > 853) {
           return (
             <div>
-              <div className="fixed left-4 inset-y-1/2">{prevButton()}</div>
-              <div className="fixed right-4 inset-y-1/2">{nextButton()}</div>
+              <div className="fixed inset-y-1/2 left-4">{prevButton()}</div>
+              <div className="fixed inset-y-1/2 right-4">{nextButton()}</div>
             </div>
           );
         } else {
