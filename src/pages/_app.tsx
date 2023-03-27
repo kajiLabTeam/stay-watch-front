327f2b72cafa304c9476f7522930247128b9dc98
happy663 <tatu2425@gmail.com>
2023-03-27 14:15:49 +0900

    fix: import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import { RecoilRoot } from 'recoil';
 import { SWRConfig } from 'swr';
+import Layout from '@/components/layout/Layout';
 import AuthEmail from '@/features/Auth/AuthEmail';
 import AuthToken from '@/features/Auth/AuthToken';
-import Layout from '@/components/layout/Layout';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
