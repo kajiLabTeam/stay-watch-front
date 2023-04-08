ebbc8e598ef7135f810c7d5573d8e79a6cdf92da
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-04-08 23:14:26 +0900

    fix: フロアマップをcanvasに描画する部分のanyの型定義を行なった

--- src/features/admin/editFloorMap/FloorMapCanvas.tsx
+++ src/features/admin/editFloorMap/FloorMapCanvas.tsx
@@ -1,36 +1,40 @@
 import { useRef, useEffect } from 'react';
 
 export const FloorMapCanvas = (props: { buildingImagePath: string }) => {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null); // canvasRef の型を MutableRefObject<HTMLCanvasElement | null> に指定
   const CANVAS_WIDTH = 2880;
   const CANVAS_HEIGHT = 1800;
 
-  const getContext = (): CanvasRenderingContext2D => {
-    const canvas: any = canvasRef.current;
+  const getContext = (): CanvasRenderingContext2D | null => {
+    const canvas: HTMLCanvasElement | null = canvasRef.current; // canvas の型を HTMLCanvasElement | null に指定
 
-    return canvas.getContext('2d');
+    return canvas ? canvas.getContext('2d') : null;
   };
 
   useEffect(() => {
     // buildingImagePathが変わった時に再レンダリング
 
     const buildingImage = new Image();
-    const ctx: CanvasRenderingContext2D = getContext();
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    const ctx: CanvasRenderingContext2D | null = getContext(); // ctx の型を CanvasRenderingContext2D | null に指定
+    if (ctx) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
     buildingImage.src = `/floor_maps${props.buildingImagePath}`;
 
     function drawBuildingToCanvas(buildingImage: HTMLImageElement) {
-      ctx.fillStyle = 'gray';
-      ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-      ctx.drawImage(
-        buildingImage,
-        1,
-        1,
-        buildingImage.width * (CANVAS_HEIGHT / buildingImage.height),
-        buildingImage.height * (CANVAS_HEIGHT / buildingImage.height),
-      );
+      if (ctx) {
+        ctx.fillStyle = 'gray';
+        ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        ctx.drawImage(
+          buildingImage,
+          1,
+          1,
+          buildingImage.width * (CANVAS_HEIGHT / buildingImage.height),
+          buildingImage.height * (CANVAS_HEIGHT / buildingImage.height),
+        );
+      }
     }
 
     buildingImage.onload = () => {
