353b8ec6726506183560e5fdd18b9c512aa788e6
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-04-25 15:34:45 +0900

    feat: zodによるバリデーション作成

--- src/features/admin/user/CreateUserForm.tsx
+++ src/features/admin/user/CreateUserForm.tsx
@@ -1,113 +1,106 @@
 import { TextInput, MultiSelect, Select } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { useForm, zodResolver } from '@mantine/form';
 import axios from 'axios';
+import { useEffect } from 'react';
+import { schema } from './hooks/shema';
 import { Button } from '@/components/common/Button';
 import { useSelectBeacons } from '@/features/admin/user/hooks/beaconSelector';
 import { useSelectTags } from '@/features/admin/user/hooks/tagSelector';
 
 import { endpoints } from '@/utils/api';
 
-
 export const CreateUserForm = () => {
-  //   const { storeRoomToDatabase } = useEditingMapMutators();
-  //   const [roomNameValue, setRoomNameValue] = useState(props.room.roomName);
   const selectBeacons = useSelectBeacons();
   const selectTags = useSelectTags();
 
-  const roles=[
-    {value:1, label:"一般ユーザ"},
-    {value:2, label:"研究室管理者"}
-  ]
+  const roles = [
+    { value: 1, label: '一般ユーザ' },
+    { value: 2, label: '研究室管理者' },
+  ];
 
   const form = useForm({
     initialValues: {
-      name:"",
-      uuid:"",
-      email:"",
-      role:1,
-      communityId:1,
-      beaconName:"",
-      tagIds  :[]
+      name: '',
+      uuid: '',
+      email: '',
+      role: 1,
+      communityId: 1,
+      beaconName: '',
+      tagIds: [],
     },
-    validate: {
-      email: (value) => (/^\S+@gmail\S+$/.test(value) ? null : 'Invalid email'),
-      name: (value) => (value ? null : 'Invalid user'),
-    },
+    validate: zodResolver(schema),
   });
 
+  useEffect(() => {
+    if (form.values.beaconName === 'FCS1301') {
+      form.setValues({ uuid: '' });
+    } else {
+      form.setValues({ uuid: '00000' });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [form.values.beaconName, form.setValues]);
+
   return (
-    <div className='border border-green-500'>
-    <div className='rounded-lg bg-slate-200'>
-      <form
-        className=' flex flex-col gap-6 p-10'
-        onSubmit={form.onSubmit((values) =>
-          // console.log(values)
-          axios
-            .post(endpoints.users2, values)
-            .then(() => {
-              window.alert('成功しました');
-            })
-            .catch((err) => {
-              if(err.response.status === 409){
-                window.alert('このメールアドレスは既に登録されています');
-              }
-              else {
-                window.alert('失敗しました');
-              }
-              console.error(err.response.status);
-            }),
-        )}
-      >
-        <TextInput
-          placeholder='tarou'
-          label='名前'
-          required
-          {...form.getInputProps('name')}
-        />
-        <TextInput
-          label='Gメールアドレス'
-          placeholder='your@gmail.com'
-          required
-          {...form.getInputProps('email')}
-        />
-        <div className="flex space-x-4">
-          <Select
-            className='w-1/2'
-            label='ビーコンの形態'
-            placeholder='ビーコンの形態を選択してください'
-            data={selectBeacons}
-            required
-            {...form.getInputProps('beaconName')}
-          />
-          <Select
-            label='権限'
-            className='w-1/2'
-            placeholder='権限レベルを選択してください'
-            data={roles}
-            required
-            {...form.getInputProps('role')}
-           />
-        </div>
-        {form.values.beaconName === "FCS1301" && (
+    <div className='mx-5'>
+      <div className='rounded-lg bg-slate-200'>
+        <form
+          className=' flex flex-col gap-2 px-10 py-4'
+          onSubmit={form.onSubmit((values) =>
+            // console.log(values)
+            axios
+              .post(endpoints.users2, values)
+              .then(() => {
+                window.alert('成功しました');
+              })
+              .catch((err) => {
+                if (err.response.status === 409) {
+                  window.alert('このメールアドレスは既に登録されています');
+                } else {
+                  window.alert('失敗しました');
+                }
+                console.error(err.response.status);
+              }),
+          )}
+        >
+          <TextInput placeholder='tarou' label='名前' {...form.getInputProps('name')} />
           <TextInput
-            label='UUID'
-            placeholder='UUID'
-            required
-            {...form.getInputProps('uuid')}
+            label='Gメールアドレス'
+            placeholder='your@gmail.com'
+            {...form.getInputProps('email')}
           />
-        ) }
-        <MultiSelect
-          label='タグ'
-          placeholder='属性を選択してください'
-          data={selectTags}
-          required
-          {...form.getInputProps('tagIds')}
-        />
-        <div className=' mx-auto'>
-          <Button color='blue'>登録する</Button>
-        </div>
-      </form>
+          <div className='flex space-x-4'>
+            <Select
+              className='w-1/2'
+              label='ビーコンの形態'
+              placeholder='ビーコンの形態を選択してください'
+              data={selectBeacons}
+              {...form.getInputProps('beaconName')}
+            />
+            <Select
+              label='権限'
+              className='w-1/2'
+              placeholder='権限レベルを選択してください'
+              data={roles}
+              {...form.getInputProps('role')}
+            />
+          </div>
+          {form.values.beaconName === 'FCS1301' && (
+            <>
+              <TextInput label='UUID(5文字)' placeholder='UUID' {...form.getInputProps('uuid')} />
+              {/* {form.setValues({ ...form.values, uuid: 'abcdf' })} */}
+            </>
+          )}
+          <MultiSelect
+            label='タグ'
+            placeholder='属性を選択してください'
+            data={selectTags}
+            {...form.getInputProps('tagIds')}
+          />
+          <div className=' mx-auto'>
+            <Button color='blue'>登録する</Button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-  )
+  );
 };
