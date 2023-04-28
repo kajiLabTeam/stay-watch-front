11b2b5235bb3b479b76de974375069059a52561d
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-04-28 17:15:56 +0900

    feat: メールアドレスのバイデーションを空で通るようにした

--- src/features/admin/user/hooks/shema.ts
+++ src/features/admin/user/hooks/shema.ts
@@ -4,7 +4,7 @@ export const schema = z.object({
   name: z
     .string()
     .min(1, { message: '12文字以内で入力してください' })
-    .max(1, { message: '12文字以内で入力してください' }),
+    .max(12, { message: '12文字以内で入力してください' }),
   uuid: z
     .string()
     .regex(new RegExp('.*[0-9a-f].*'), { message: '0〜9とa〜fで入力してください' })
@@ -12,8 +12,9 @@ export const schema = z.object({
     .max(5, { message: '5文字で入力してください' }),
   email: z
     .string()
-    .email({ message: '有効なメールアドレスを入力してください' })
-    .min(1, { message: '必須項目です' }),
+    .regex(new RegExp('^$|@gmail.com$'), { message: '〜@gmail.comの形式で入力してください' }),
+  // .email({ message: '有効なメールアドレスを入力してください' })
+  // .min(1, { message: '必須項目です' }),
   role: z.number(),
   communityId: z.number(),
   beaconName: z.string().min(1, { message: '必須項目です' }),
