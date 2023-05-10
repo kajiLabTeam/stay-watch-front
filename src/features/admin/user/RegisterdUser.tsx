ec23c9308697459073307c819f914610c1cca36f
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-05-10 16:30:05 +0900

    style: 不要なコメント分の削除

--- src/features/admin/user/RegisterdUser.tsx
+++ src/features/admin/user/RegisterdUser.tsx
@@ -45,27 +45,4 @@ export const RegisterdUser = (props: {
       )}
     </>
   );
-
-  // if (props.user.id === editingUserId) {
-  //   return (
-  //     <tr className='text-left' key={props.user.id}>
-  //       <td colSpan={5}>
-  //         <EditUserForm user={props.user} />
-  //       </td>
-  //     </tr>
-  //   );
-  // } else if (props.user.id !== editingUserId) {
-  //   return (
-  //     <tr className='text-left' key={props.user.id} onClick={() => setEditingUserId(props.user.id)}>
-  //       <td className='min-w-fit whitespace-nowrap border py-1 px-4'>{props.user.name}</td>
-  //       <td className='min-w-fit whitespace-nowrap border py-1 px-4'>{props.user.uuid}</td>
-  //       <td className='min-w-fit whitespace-nowrap border py-1 px-4'>
-  //         {roles[props.user.role - 1].label}
-  //       </td>
-  //       <td className='min-w-fit whitespace-nowrap border py-1 px-4'>{props.user.beaconName}</td>
-  //       <td className='whitespace-nowrap border py-1 px-4'>{props.user.tags[0].name}</td>
-  //     </tr>
-  //   );
-  // }
-  // return <div>a</div>;
 };
