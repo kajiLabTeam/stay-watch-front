5c5780c5636be380d07bfe64b19204516845510f
happy663 <tatu2425@gmail.com>
2023-05-13 14:02:24 +0900

    fix: rewritesの書き方を変更

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: [
-    {
-      source: "/__/auth/:path*",
-      destination: `https://${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}/__/auth/:path*`,        
-    }
-  ]
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com/__/auth/:path*`,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
