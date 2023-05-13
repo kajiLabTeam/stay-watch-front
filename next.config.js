6848aa6a8d3a043b9a15864f44d12254ec285176
happy663 <tatu2425@gmail.com>
2023-05-13 13:43:49 +0900

    add: next.config.jsにrewritesという特定のURLに対してリダイレクトできる設定を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: [
+    {
+      source: "/__/auth/:path*",
+      destination: `https://${firebaseAuthDomain}/__/auth/:path*`,        
+    }
+  ]
 }
 
 module.exports = nextConfig
