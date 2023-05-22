0135615bf240094a1d918e0d9b7b7146b88b39ff
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-05-22 19:13:53 +0900

    fix: 他ファイルに切り出していた処理を戻してみた

--- src/features/floorMap/FloorMap.tsx
+++ src/features/floorMap/FloorMap.tsx
@@ -1,10 +1,56 @@
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import { SizeMe } from 'react-sizeme';
 import PopoverTop from '@/features/floorMap/PopoverTop';
-import { useRoomState } from '@/features/floorMap/roomState';
+// import { useRoomState } from '@/features/floorMap/roomState';
+import { useCommunityState } from '@/globalStates/useCommunityState';
+import { useSuspenseSWR } from '@/hooks/useSuspenseSWR';
+import { EditorRoom } from '@/types/roomFloormap';
+import RoomInformation from '@/types/roomInformation';
+import RoomStatus from '@/types/roomStatus';
+import StayerType from '@/types/stayer';
+import { endpoints } from '@/utils/api';
 
 export const FloorMap = () => {
-  const { roomsStatus, roomInformation } = useRoomState();
+  const community = useCommunityState();
+  const { data: rooms } = useSuspenseSWR<EditorRoom[]>(
+    `${endpoints.getRoomsEditorByCommunityID}/${community.communityId}`,
+  );
+  const { data: stayers } = useSuspenseSWR<StayerType[]>(`${endpoints.stayers}`);
+  const [roomsStatus, setRoomsStatus] = useState<RoomStatus[]>([]);
+  const [roomInformation, setRoomInformation] = useState<RoomInformation[]>([
+    { roomID: 1, roomName: '', top: 0, left: 0 },
+  ]);
+
+  useEffect(() => {
+    if (rooms && stayers) {
+      const tmpRoomsStatus: RoomStatus[] = [];
+      const tmpRoomsInformation: RoomInformation[] = [];
+      rooms.forEach((room) => {
+        const tmpRoomStatus: RoomStatus = {
+          roomID: room.roomId,
+          userCount: 0,
+          usersName: [],
+        };
+        const tmpRoomInformation: RoomInformation = {
+          roomID: room.roomId,
+          roomName: room.roomName,
+          top: (room.polygon[0][1] + room.polygon[1][1]) / 36,
+          left: (room.polygon[0][0] + room.polygon[1][0]) / 58,
+        };
+        tmpRoomsInformation.push(tmpRoomInformation);
+        stayers.forEach((stayer) => {
+          if (stayer.roomId === room.roomId) {
+            tmpRoomStatus.userCount += 1;
+            tmpRoomStatus.usersName.push(stayer.name);
+          }
+        });
+        tmpRoomsStatus.push(tmpRoomStatus);
+      });
+      setRoomsStatus(tmpRoomsStatus);
+      setRoomInformation(tmpRoomsInformation);
+    }
+  }, [rooms, stayers]);
 
   return (
     <SizeMe monitorHeight monitorWidth>
