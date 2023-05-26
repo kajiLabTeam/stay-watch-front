4b985a78c522d11b221979b04b4ecb0580a32425
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-05-26 15:08:09 +0900

    refactor: 不要なconsole.logの削除。コメント文の追加。

--- src/features/floorMap/useFloorMapCanvas.tsx
+++ src/features/floorMap/useFloorMapCanvas.tsx
@@ -22,15 +22,12 @@ export const useFloorMapCanvas = () => {
     const ctx: CanvasRenderingContext2D | null = getContext(); // ctx の型を CanvasRenderingContext2D | null に指定
     if (ctx && viewerRooms) {
       // 初期化処理
-      console.log('初期化開始');
       ctx.fillStyle = 'white';
       ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
       ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-      console.log('初期化終了');
       buildingImage.src = `/floor_maps/4g-honkan-bekkan.jpg`;
 
       const drawBuildingToCanvas = () => {
-        console.log('drawBuildingToCanvas開始');
         ctx.fillStyle = 'gray';
         ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         ctx.drawImage(
@@ -40,22 +37,16 @@ export const useFloorMapCanvas = () => {
           buildingImage.width * (CANVAS_HEIGHT / buildingImage.height),
           buildingImage.height * (CANVAS_HEIGHT / buildingImage.height),
         );
-        console.log('drawBuildingToCanvas終了');
       };
 
       const drawUsersToCanvas = () => {
-        console.log('drawUsersToCanvas開始');
-        console.log(viewerRooms);
         viewerRooms.map((viewerRoom) => {
-          // console.log(roomInformation);
-          // console.log('1描きよ');
           ctx.fillStyle = 'red';
           ctx.font = '48px serif';
           ctx.textAlign = 'center';
           ctx.textBaseline = 'middle';
           ctx.fillText(String(viewerRoom.userCount), viewerRoom.left, viewerRoom.top);
         });
-        console.log('drawUsersToCanvas終了');
       };
       buildingImage.onload = () => {
         drawBuildingToCanvas();
@@ -66,25 +57,22 @@ export const useFloorMapCanvas = () => {
 
   // canvas上でマウスが動いた時の処理
   const displayPopover = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    // console.log('要素での座標');
-    // console.log(e.clientX);
-    // console.log(e.clientY);
+    // e.clientX, e.clientY : 要素での座標(画面サイズに依存する)
+    // clientCanvasX, clientCanvasY: canvas上での座標(画面サイズに依存しない)
     const canvasElementRatio = canvasRef.current!.clientWidth / CANVAS_WIDTH;
     const rect = canvasRef.current?.getBoundingClientRect();
-    // console.log('canvas内での座標');
+    // canvas上でのマウスの座標を定義
     const clientCanvasX = Math.trunc((e.clientX - Math.floor(rect!.left)) / canvasElementRatio);
     const clientCanvasY = Math.trunc((e.clientY - Math.floor(rect!.top)) / canvasElementRatio);
-    // console.log(clientCanvasX);
-    // console.log(clientCanvasY);
-    let clickedRoom = false;
+
+    let isMouseOverRoom = false;
     viewerRooms?.map((viewerRoom) => {
       // ?なのはviewerRoomsがnullだったらcanvasの初期化が行われないためdisplayPopvoerがそもそも動かない
       if (
         Math.abs(clientCanvasX - viewerRoom.left) < 50 &&
         Math.abs(clientCanvasY - viewerRoom.top) < 50
       ) {
-        // console.log(viewingRoomId);
-        clickedRoom = true;
+        isMouseOverRoom = true;
         setPopoverRoom({
           roomId: viewerRoom.roomId,
           roomName: viewerRoom.roomName,
@@ -95,7 +83,8 @@ export const useFloorMapCanvas = () => {
         setViewingRoomId(viewerRoom.roomId);
       }
     });
-    if (!clickedRoom) {
+    if (!isMouseOverRoom) {
+      // マウスが何の部屋にも乗っていなかったらviewingRoomIdを0にする
       setViewingRoomId(0);
     }
   };
