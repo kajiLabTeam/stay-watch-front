dd3b82ee5e21367d40797dc1068a200ed9dbc300
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-05-31 12:13:12 +0900

    fix: ポップオーバーのユーザ名表示のkeyをユニークな値であるindexへ変更

--- src/features/floorMap/PopoverUser.tsx
+++ src/features/floorMap/PopoverUser.tsx
@@ -8,7 +8,7 @@ const PopoverUser = ({ roomName, userNames }: Props) => {
     <div className='mt-2 min-w-fit whitespace-nowrap border-2 border-gray-500 bg-white p-[5%] shadow-md'>
       <p className='text-gray-500'>{roomName}</p>
       {userNames.map((userName: string) => (
-        <p key={userName}>{userName}</p>
+        <p key={userNames.indexOf(userName)}>{userName}</p>
       ))}
     </div>
   );
