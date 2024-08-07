36f788c6ee585722492b4990a6ea5be5753ed29a
togawa427 <95838999+togawa427@users.noreply.github.com>
2024-08-07 16:04:12 +0900

    chore: pagesディレクトリを削除

--- src/utils/$path.ts
+++
@@ -1,9 +0,0 @@
-export const pagesPath = {
-  "admin": {
-    "kesuyo": {
-      $url: (url?: { hash?: string }) => ({ pathname: '/admin/kesuyo' as const, hash: url?.hash })
-    }
-  }
-}
-
-export type PagesPath = typeof pagesPath
