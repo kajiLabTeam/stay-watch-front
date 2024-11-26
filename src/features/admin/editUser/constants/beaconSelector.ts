c1efe0ca6a25a70bdbee8651584cd681d45a6ce8
togawa427 <95838999+togawa427@users.noreply.github.com>
2024-11-26 14:25:54 +0900

    feat: 滞在ウォッチビーコンの場合管理者の編集画面でSelecterに表示されるようにした

--- src/features/admin/editUser/constants/beaconSelector.ts
+++ src/features/admin/editUser/constants/beaconSelector.ts
@@ -2,4 +2,5 @@ export const beaconSelector = [
   { value: 'FCS1301', label: 'FCS1301' },
   { value: 'Android', label: 'Android' },
   { value: 'iPhone', label: 'iPhone' },
+  { value: 'StayWatchBeacon', label: 'StayWatchBeacon', disabled: true},
 ] as const;
