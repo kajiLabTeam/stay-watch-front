031005a0eba6008674a29187e48b332dd0915502
togawa427 <95838999+togawa427@users.noreply.github.com>
2025-07-06 02:10:38 +0900

    feat: 認証付きAPIに対応させた

--- src/hooks/useGetAPI.ts
+++ src/hooks/useGetAPI.ts
@@ -1,10 +1,26 @@
 import useSWR from 'swr';
-
-const fetcher = async (url: string) => {
-  return await fetch(url).then((res) => res.json());
-};
+import { useUserState } from '@/globalStates/firebaseUserState';
 
 export const useGetAPI = <T>(url: string) => {
+  const user = useUserState(); // useSWR の外で呼び出す
+
+  // フェッチ関数
+  const fetcher = async (url: string) => {
+    if (!user) throw new Error('Not Found User'); // ユーザが存在しない場合エラーを投げる
+    const token = await user.getIdToken();
+
+    // 認証トークン付きでリクエストを送信
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!res.ok) throw new Error('API failed');
+    return res.json();
+  };
+
+  // SWRを使ってデータ取得とキャッシュ
   const { data, error, isLoading } = useSWR<T>(url, fetcher);
-  return { data: data, error, isLoading };
+
+  return { data, error, isLoading };
 };
