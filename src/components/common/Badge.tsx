a34ad71aa899217acaf912a523ab5c1adda529b7
togawa427 <95838999+togawa427@users.noreply.github.com>
2026-03-08 23:06:34 +0900

    style: タグを表示するコンポーネントのパディング調整

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -6,7 +6,12 @@ type Props = {
 
 export const Badge = ({ name }: Props) => {
   return (
-    <span className='h-fit rounded-full bg-staywatch-accent/10 px-1 text-xs text-staywatch-accent md:px-2 md:text-base'>
+    <span
+      className='
+      h-fit rounded-full bg-staywatch-accent/10 px-1.5 py-px text-xs text-staywatch-accent
+      md:px-2 md:py-0.5 md:text-base
+    '
+    >
       {name}
     </span>
   );
