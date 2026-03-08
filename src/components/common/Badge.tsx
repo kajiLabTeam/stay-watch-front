e50bed16281a8c1536968578d3dade6be9633399
togawa427 <95838999+togawa427@users.noreply.github.com>
2026-03-08 22:39:52 +0900

    feat: バッジコンポーネントをインライン利用を考えているため div ではなく span にした

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export const Badge = ({ name }: Props) => {
   return (
-    <div className='h-fit rounded-full bg-staywatch-accent/10 px-1 text-xs text-staywatch-accent md:px-2 md:text-base'>
+    <span className='h-fit rounded-full bg-staywatch-accent/10 px-1 text-xs text-staywatch-accent md:px-2 md:text-base'>
       {name}
-    </div>
+    </span>
   );
 };
